c44b268d6f760548b5482a973da698c6e8149cfe
Andrei Beliaev <andrewbelyaev2164@gmail.com>
2025-11-18 21:28:33 -0600

    Remove focus ring from toggle switch

--- components/ui/switch.tsx
+++ components/ui/switch.tsx
@@ -13,7 +13,7 @@ function Switch({
     <SwitchPrimitive.Root
       data-slot="switch"
       className={cn(
-        "peer inline-flex h-[1.15rem] w-8 shrink-0 items-center rounded-full border border-transparent shadow-sm transition-all outline-none focus-visible:ring-2 focus-visible:ring-gray-400 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        "peer inline-flex h-[1.15rem] w-8 shrink-0 items-center rounded-full border border-transparent shadow-sm transition-all outline-none disabled:cursor-not-allowed disabled:opacity-50",
         "data-[state=checked]:bg-gray-700 data-[state=unchecked]:bg-gray-200",
         className
       )}
